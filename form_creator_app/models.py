c7e7e7e93c7b932dafa4769253e9ef979d1c1581
Denis Ivanov <ledorubden@gmail.com>
2022-05-06 22:58:00 +0300

    Implement models.

--- form_creator_app/models.py
+++ form_creator_app/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from form_creator_app.utils import aware_utc_now
+
+
+class FormField(models.Model):
+    TYPE_CHOICES = (
+        ('input', 'Input'),
+        ('textarea', 'Textarea'),
+        ('select', 'Select')
+    )
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    required = models.BooleanField(default=False, blank=True)
+    form = models.ForeignKey(
+        'Form',
+        related_name='fields',
+        on_delete=models.CASCADE
+    )
+
+
+class FieldChoice(models.Model):
+    name = models.CharField(max_length=100)
+    field = models.ForeignKey(
+        'FormField',
+        related_name='choices',
+        on_delete=models.CASCADE
+    )
+
+
+class Form(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4)
+    title = models.CharField(max_length=100)
+    date_created = models.DateTimeField(default=aware_utc_now)
+
+
+class FormEntry(models.Model):
+    form = models.ForeignKey(
+        'Form',
+        related_name='entries',
+        on_delete=models.CASCADE
+    )
+
+
+class FormFieldEntry(models.Model):
+    field = models.ForeignKey(
+        'FormField',
+        related_name='entries',
+        on_delete=models.CASCADE
+    )
